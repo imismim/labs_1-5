0019643331b4b45a4cd3074848bf3ef9faebf116
Imismim <imismims@gmail.com>
2022-11-12 22:29:45 +0200

    lab number 2 list

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def lab_1():
 
     print(f"Result: {result}")
 
+
+def lab_2():
+    numbers = list(map(int, input("Enter numbers: ").split(" ")))
+    numbers_count = sorted([[number, numbers.count(number)] for number in set(numbers)], key=lambda x: x[1],
+                           reverse=True)
+    numbers_same_count = sorted([number for number in numbers_count if number[1] == numbers_count[0][1]])
+    print(f"the most common: {numbers_same_count[0][0]}")
